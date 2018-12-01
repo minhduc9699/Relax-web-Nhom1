4f5710fcd3237133498b975c9d14586e809ee4d6
yoloteam456 <definitelynotduong@gmail.com>
2018-12-01 20:41:38 +0700

    d

--- web.js
+++ web.js
@@ -1,28 +1,7 @@
 var DOMhello = document.getElementById('hello');
-var DOMbtn = document.getElementsByClassName('btnn');
-// console.log(DOMhello);
-console.log(DOMbtn);
-
-
-function Text1() {
+console.log(DOMhello);
+setTimeout(function(){
     DOMhello.classList.remove('fadeInRight');
     DOMhello.classList.add('fadeOut');
-}
-
-setTimeout(function(){
-    Text1();
-    setTimeout(function(){
-        DOMhello.innerText = "It seems that you are...";
-        DOMhello.classList.remove('fadeOut');
-        DOMhello.classList.add('fadeInRight');
-        setTimeout(function(){
-            for (var i = 0; i < DOMbtn.length; i ++) {
-                DOMbtn[i].style.opacity = "1";
-            }
-            
-        }, 2000)
-    }, 1000)
-}, 1000)
-
-
-
+    DOMhello.style.left = '50%';
+}, 2000)
