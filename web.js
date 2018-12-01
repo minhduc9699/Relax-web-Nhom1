fbc59fa8a57233b8e5740f134971fa3981e35fbe
bui anh nhat <buianhnhat2003@gmail.com>
2018-12-01 20:24:40 +0700

    .

--- web.js
+++ web.js
@@ -1,5 +1,8 @@
 var DOMhello = document.getElementById('hello');
-console.log(DOMhello);
+var DOMbtn = document.getElementsByClassName('btnn');
+// console.log(DOMhello);
+console.log(DOMbtn);
+
 
 function Text1() {
     DOMhello.classList.remove('fadeInRight');
@@ -12,8 +15,14 @@ setTimeout(function(){
         DOMhello.innerText = "It seems that you are...";
         DOMhello.classList.remove('fadeOut');
         DOMhello.classList.add('fadeInRight');
-    }, 4000)
-}, 4000)
+        setTimeout(function(){
+            for (var i = 0; i < DOMbtn.length; i ++) {
+                DOMbtn[i].style.opacity = "1";
+            }
+            
+        }, 2000)
+    }, 1000)
+}, 1000)
 
 
 
