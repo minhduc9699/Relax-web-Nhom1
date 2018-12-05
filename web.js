484c408214bfdf5e5acd1e91171f08630e4c2236
yoloteam456 <definitelynotduong@gmail.com>
2018-12-05 20:15:34 +0700

    d

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var DOMhello = document.getElementById('hello');
 var DOMbtn = document.getElementsByClassName('btnn');
 // console.log(DOMhello);
-console.log(DOMbtn);
 
 
 function Text1() {
@@ -20,11 +19,43 @@ setTimeout(function(){
                 var DOMbtnn = DOMbtn[i];
                 DOMbtnn.style.opacity = "1";
                 DOMbtnn.addEventListener('click', function(e){
-                    console.log(e.target);
                     DOMbtnID = document.getElementById(e.target.id);
                     DOMbtnID.style.height = "100%";
                     DOMbtnID.style.width = "100%";
-    
+                    if(e.target.id == "btn1") {
+                        var h = e.target.parentNode;
+                        h.querySelector("#btn4").remove();
+                        h.querySelector("#btn2").remove();
+                        h.querySelector("#btn3").remove();
+                        var delaback = document.getElementById('bg');
+                        delaback.style.background='none';
+                    }
+                    if(e.target.id == "btn2") {
+                        var h = e.target.parentNode;
+                        h.querySelector("#btn4").remove();
+                        h.querySelector("#btn1").remove();
+                        h.querySelector("#btn3").remove();
+                        var delaback = document.getElementById('bg');
+                        delaback.style.background='none';
+                    }
+                    if(e.target.id == "btn3") {
+                        var h = e.target.parentNode;
+                        h.querySelector("#btn2").remove();
+                        h.querySelector("#btn1").remove();
+                        h.querySelector("#btn4").remove();
+                        var delaback = document.getElementById('bg');
+                        delaback.style.background='none';
+                    }
+                    if(e.target.id == "btn4") {
+                        var h = e.target.parentNode;
+                        h.querySelector("#btn2").remove();
+                        h.querySelector("#btn1").remove();
+                        h.querySelector("#btn3").remove();
+                        var delaback = document.getElementById('bg');
+                        delaback.style.background='none';
+                    }
+                        
+                        
                 })
             }
         }, 4000)
